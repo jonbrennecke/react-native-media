609f083d7e59ca0088b6cbc6d53c40f5b145ab67
Jonathan Brennecke <jbrennecke@opengov.com>
2019-06-08 12:27:04 -0700

    added a few basic deps: flow, eslint, babel

--- ios/RNJonbrenneckeFarrago.h
+++ ios/RNJonbrenneckeFarrago.h
@@ -8,4 +8,3 @@
 @interface RNJonbrenneckeFarrago : NSObject <RCTBridgeModule>
 
 @end
-  
--- ios/RNJonbrenneckeFarrago.m
+++ ios/RNJonbrenneckeFarrago.m
@@ -3,11 +3,9 @@
 
 @implementation RNJonbrenneckeFarrago
 
-- (dispatch_queue_t)methodQueue
-{
-    return dispatch_get_main_queue();
+- (dispatch_queue_t)methodQueue {
+  return dispatch_get_main_queue();
 }
 RCT_EXPORT_MODULE()
 
 @end
-  
